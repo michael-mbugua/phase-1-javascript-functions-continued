41b5c8fdecd3c0dbdcd20e88c8d13080dde4bc3d
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-17 15:07:57 +0300

    completed labs for javascript functions by creating functions and returning functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const mondayWork = function(activity){
+    console.log("this monday i will" + " " + activity)
+}
+mondayWork("go to the office")
+
+const saturdayFun=function( activity2){
+    console.log("this saturday i will" + " " + activity2)
+}
+saturdayFun("roller-skate")
+
+function wrapAdjective(string1){
+    return function (string2){
+        console.log("you are " + " "+ string1 +" " + string2)
+    }
+    
+}
+wrapAdjective("%a dedicated")("programmer");
